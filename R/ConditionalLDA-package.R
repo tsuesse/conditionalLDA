3b0e25face58998158abf0f07de6be202335a27f
Thomas Suesse <tsuesse@uow.edu.au>
2023-02-27 15:17:06 +0100

    Init prject

---
+++ R/ConditionalLDA-package.R
@@ -0,0 +1,12 @@
+#' Spatial LDA and conditional prediction
+#'
+#' This package implements spatial LDA model fit including covariance function
+#' modelling. Predictions can include nearby training observations by means of a
+#' conditional multivariate normal distribution (conditional prediction).
+#' Predictions can be made for a whole block instead of single observations if
+#' block identifiers are given.
+#'
+#' @docType package
+#' @name conditionalLDA-package
+#' @aliases conditionalLDA-package
+NULL
